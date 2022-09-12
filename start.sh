e25af7a374bd80280c44fe849a468966b18d7fdf
Devin Price <devin@wptheming.com>
2022-09-11 21:14:03 -0500

    Working sed.

--- start.sh
+++ start.sh
@@ -4,14 +4,21 @@
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+initial_slug="devpress-woocommerce-boilerplate"
+initial_name="DevPress WooCommerce Boilerplate"
+
 echo ""
 echo "Hello! Let's get started with your WooCommerce plugin."
 echo ""
-echo "${bold}What is the slug for your plugin?${normal} (e.g. devpress-example-plugin)"
+echo "${bold}What is the slug for your plugin?${normal} (e.g. ${initial_slug})"
 read -p 'Slug: ' slug
 echo ""
-echo "${bold}What is the name of your plugin?${normal} (e.g. DevPress Example Plugin)"
+echo "${bold}What is the name of your plugin?${normal} (e.g. ${initial_name})"
 read -p 'Name: ' name
 echo ""
 
-echo "Setting up $name with slug $slug..."
+echo "Setting up ${bold}${name}${normal} (${slug})..."
+echo ""
+
+sed -i '' "s/$initial_slug/$slug/" *.txt
+sed -i '' "s/$initial_name/$name/" *.txt
